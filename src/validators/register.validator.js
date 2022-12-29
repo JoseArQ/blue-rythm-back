79ccf2eef09f1bb432010c81862b83c1412c804f
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 18:48:57 -0500

    feat: add avatar validation

--- src/validators/register.validator.js
+++ src/validators/register.validator.js
@@ -4,7 +4,8 @@ const registerSchema = Joi.object({
     name: Joi.string().min(5).max(20).required(),
     surname: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(50).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(5).max(255).required(),
+    avatar: Joi.string().valid('avatar1', 'avatar2', 'avatar3')
 });
 
 export default registerSchema;
