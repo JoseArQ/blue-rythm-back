5d6c0170b965bf1806fd0322bb1ac46585da7006
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 18:48:12 -0500

    feat: add update user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import Validator from "../middleware/validators.js";
+import auth from "../middleware/auth.js";
 import userRegisterController from "../controllers/user-register.cotroller.js";
 import userLoginController from "../controllers/user-login.controller.js";
+import userUpdateController from "../controllers/user-update.controller.js";
 
 const router = Router();
 
 router.post('/register', Validator('register'), userRegisterController);
 router.post('/login', userLoginController);
-// router.patch('/update-data');
-// router.patch('/update-email');
-// router.patch('/update-password');
+router.patch('/', auth, Validator('update'), userUpdateController);
 // router.delete('/unregister');
 
 export default router;
