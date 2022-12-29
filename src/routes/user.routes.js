2b6adf566d3907c293cfe9ed27b1da36b51715a8
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 12:08:28 -0500

    feat: add controller in login route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import Validator from "../middleware/validators.js";
 import userRegisterController from "../controllers/user-register.cotroller.js";
+import userLoginController from "../controllers/user-login.controller.js";
 
 const router = Router();
 
 router.post('/register', Validator('register'), userRegisterController);
-// router.post('/login');
+router.post('/login', userLoginController);
 // router.patch('/update-data');
 // router.patch('/update-email');
 // router.patch('/update-password');
