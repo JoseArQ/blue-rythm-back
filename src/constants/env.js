21f168761198c0383d5d67ccd265ec26eb2ba02d
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 11:58:36 -0500

    feat: set NODE_ENV like a constants

---
+++ src/constants/env.js
@@ -0,0 +1,6 @@
+
+export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || 'privateKey';
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = process.env.PORT;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
