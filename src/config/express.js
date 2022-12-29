8ff0af1acf0cdd30024fe75b8cd89632686007be
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 12:12:19 -0500

    feat: add sessions

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import session from 'express-session';
 import userRouter from '../routes/user.routes.js';
 
 const expressApp = express();
 
 // Midleware
 expressApp.use(express.json());
+expressApp.use(session({
+    secret: "secrete-key",
+    saveUninitialized: false,
+    resave: true
+}));
 
 // Routes
 expressApp.use('/api/user', userRouter);
