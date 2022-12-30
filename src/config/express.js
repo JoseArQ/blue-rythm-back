8e75b3e8ede9773428f3250938cb1377767cda0d
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 19:20:05 -0500

    feat: add secret env for session

--- src/config/express.js
+++ src/config/express.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import session from 'express-session';
 import userRouter from '../routes/user.routes.js';
+import searchRouter from '../routes/search.routes.js';
+
+import { SESSION_SECRET } from "../constants/env.js";
 
 const expressApp = express();
 
 // Midleware
 expressApp.use(express.json());
 expressApp.use(session({
-    secret: "secrete-key",
+    secret: SESSION_SECRET, 
     saveUninitialized: false,
     resave: true
 }));
 
 // Routes
 expressApp.use('/api/user', userRouter);
+expressApp.use('/api/search', searchRouter);
 
 export default expressApp;
--- src/constants/env.js
+++ src/constants/env.js
@@ -4,3 +4,4 @@ export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT;
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const SESSION_SECRET = process.env.SESSION_SECRET;
