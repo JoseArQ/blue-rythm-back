de00b5fea86245fdf408dff8859265dd206a8c96
Jose Arroyave <arquos.14@gmail.com>
2022-12-29 20:00:30 -0500

    feat: setting session for production

--- src/config/express.js
+++ src/config/express.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import userRouter from '../routes/user.routes.js';
 import searchRouter from '../routes/search.routes.js';
 
 import { SESSION_SECRET } from "../constants/env.js";
+import { MONGODB_URI } from "../constants/env.js";
 
 const expressApp = express();
 
-// Midleware
-expressApp.use(express.json());
-expressApp.use(session({
+const sess = {
     secret: SESSION_SECRET, 
     saveUninitialized: false,
     resave: true
-}));
+}
+
+if (expressApp.get('env') === 'production') {
+    expressApp.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+    sess.store = MongoStore.create({ mongoUrl: MONGODB_URI });
+}
+
+// Midleware
+expressApp.use(express.json());
+expressApp.use(session(sess));
 
 // Routes
 expressApp.use('/api/user', userRouter);
