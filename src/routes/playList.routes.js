60a3689cf0cc71de277fad98a024dabacd4e91c3
Jose Arroyave <arquos.14@gmail.com>
2023-01-16 16:51:35 -0500

    feat: add patch play list route

--- src/routes/playList.routes.js
+++ src/routes/playList.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import tokenVerify from "../middleware/tokenVerify.js";
+import Validator from "../middleware/validators.js";
 import playListController from "../controllers/playList-create.controller.js"
 import { getPlayListController, getPlayListByIdController } from "../controllers/playList-get.controller.js";
 import { getPublicPlayListController } from "../controllers/playList-publics.controller.js";
+import { updatePlayList } from "../controllers/playList-update.controller.js"
 import auth from "../middleware/auth.js";
 
 const playListRouter = Router();
@@ -12,5 +14,6 @@ playListRouter.post("/", auth, tokenVerify, playListController);
 playListRouter.get("/", auth, getPlayListController);
 playListRouter.get("/publics", getPublicPlayListController);
 playListRouter.get("/:id", getPlayListByIdController);
+playListRouter.patch("/:id", auth, tokenVerify, Validator('playlist'), updatePlayList);
 
 export default playListRouter;
